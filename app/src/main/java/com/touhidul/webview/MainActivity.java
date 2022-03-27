506b7f1d0778f22670ec6d76c023b9c2e92a127b
Touhidul islam <touhidulislam066@gmail.com>
2022-03-27 21:39:49 +0600

    'complite'

---
+++ app/src/main/java/com/touhidul/webview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.touhidul.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://boiferry.com/");
+    }
+}
